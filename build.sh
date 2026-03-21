a2d4915dafe3b7a7aa6ca159e2a5cedbeec7ba41
My Mac <mymac@Mys-MacBook-Pro.local>
2026-03-21 15:45:48 +0300

    Add setuptools and missing BeatThis dependencies

--- build.sh
+++ build.sh
@@ -12,11 +12,14 @@ pip install Cython numpy==1.20.3
 # Install madmom using --no-build-isolation
 pip install madmom==0.16.1 --no-build-isolation
 
-# Install BeatNet from the correct repository (mjhydri)
+# Install BeatNet from the correct repository
 pip install git+https://github.com/mjhydri/BeatNet.git --no-deps
 
 # Install BeatThis
 pip install git+https://github.com/CPJKU/beat_this.git --no-deps
+
+# Install missing BeatThis dependencies
+pip install einops rotary-embedding-torch
 
 # Install remaining requirements
 pip install -r requirements.txt
