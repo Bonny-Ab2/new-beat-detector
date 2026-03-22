6511ef8992c5abfc534efa6c26355ed9906de3f1
My Mac <mymac@Mys-MacBook-Pro.local>
2026-03-22 07:49:48 +0300

    Update build.sh to install gunicorn

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ pip install numpy==1.24.3
 pip install git+https://github.com/CPJKU/beat_this.git
 pip install einops rotary-embedding-torch
 pip install flask==2.3.3 flask-cors==4.0.0 gunicorn==21.2.0 librosa==0.10.1
+
+# Ensure gunicorn is available
+which gunicorn
+gunicorn --version
